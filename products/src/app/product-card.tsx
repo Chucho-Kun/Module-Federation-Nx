ac0c3464825751f19809ee3b95b069109b773157
Jesus Peralta <desarrolladorwebodm@gmail.com>
2025-12-15 17:01:54 -0600

    first button

--- products/src/app/product-card.tsx
+++ products/src/app/product-card.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 export default function ProductCard() {
+
+  const[count, setCount] = useState(0);
+
+  function incrementar() {
+    setCount(count + 1);
+  }
+
+
 return (
-     <div style={{ padding: 20, border: '1px solid #ccc' }}>
+    <div style={{ padding: 20, border: '1px solid #ccc' }}>
       <h2>Producto desde Remote</h2>
+      <button onClick={incrementar}>Totales: {count}</button>
     </div>
 )
 }
